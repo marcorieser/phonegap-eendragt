20429072630f70e3280b6ea325f01d5d28f40edc
Marco Rieser <marco.rieser@jedesign.ch>
2015-03-21 15:49:00 +0100

    3 Master vertikal und horizontal ausgerichtet

--- client/eendragt/engine/services/config.js
+++ client/eendragt/engine/services/config.js
@@ -7,9 +7,9 @@ angular.module('eendragt.engine.services.config', [])
                 y: 9
             },
             ships: [
-                { name: 'master4', elements: 4 },
+                //{ name: 'master4', elements: 4 },
                 { name: 'master3', elements: 3 },
-                { name: 'master3', elements: 3 },
+                //{ name: 'master3', elements: 3 },
                 { name: 'master2', elements: 2 },
                 { name: 'master2', elements: 2 }
             ]
